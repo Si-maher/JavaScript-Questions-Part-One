03800eae1ccfd4c339ac89c9da385d535d776a78
Simon Maher <simonmaher8307@hotmail.com>
2019-10-16 14:29:08 +0300

    added question two, check for the longest word

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 let textOne = 'wise men talk because they have something to say; fools, because they have to say something'
 
+let textTwo = 'only the dead have seen the end of war'
+
 function capitalizeWords (sentence) {
     let words = sentence.split(" ")
     let newString = []
@@ -13,3 +15,18 @@ function capitalizeWords (sentence) {
 }
 console.log(capitalizeWords(textOne));
 
+// 2) Create a function that checks the text and returns the word with the longest length.
+
+let textThree = 'The beginning is the most important part of work'
+
+function longestWord (sentence) {
+    let words1 = sentence.split(" ")
+    let longestWord = ''
+    for (i = 0; i < words1.length; i ++){
+        if (words1[i].length > longestWord.length) {
+            longestWord = words1[i]
+        }
+    }
+    return longestWord
+}
+console.log(longestWord(textThree));
